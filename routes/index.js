1e1aa9fc3e0e24c8e0d5cb4ed9cd6ee38509ee44
Chukwudi ike-Nwako <ikenwakochukwudi@gmail.com>
2023-10-02 04:53:09 -0700

    added routes for the connection and disconnection of a user and a route to display the user information

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 const indexRouter = express.Router();
 
@@ -12,5 +13,14 @@ indexRouter.get('/stats', AppController.getStats);
 
 // Add user Route
 indexRouter.post('/users', UsersController.postNew);
+
+// User Login Route
+indexRouter.get('/connect', AuthController.getConnect);
+
+// User Logout Route
+indexRouter.get('/disconnect', AuthController.getDisconnect);
+
+// User Information Route
+indexRouter.get('/users/me', UsersController.getMe);
 
 export default indexRouter;
